4845185143ab6deace1bbee029290bfa82bf2505
afsal <madathingalafsal@gmail.com>
2024-06-01 14:06:07 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Login/Sign Up</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
